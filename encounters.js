d3493f63d6068bd0d29c9c99c0d9a34405741013
Taylor <alex_taylor09@hotmail.com>
2018-11-22 18:23:30 +1100

    added fight encounter

--- encounters.js
+++ encounters.js
@@ -22,3 +22,65 @@ let sneakEncounter = (heroes, enemies) => {
     return sneakPower >= sneakBarrier
 }
 
+
+const fightEncounter = (heroes, enemies, heroesFirst) => {
+    let fighting = true
+    let totalHeroes = heroes.length
+    let totalEnemies = enemies.length
+    while(fighting){
+        if(heroesFirst){
+            totalEnemies -= teamAttack(heroes, enemies)
+            totalHeroes -= teamAttack(enemies, heroes)
+        } else {
+            totalHeroes -= teamAttack(enemies, heroes)
+            totalEnemies -= teamAttack(heroes, enemies)
+        }
+        if(totalHeroes === 0){
+            console.log(`All heroes are incapacitated.`)
+            return false
+        }
+        if(totalEnemies === 0){
+            console.log(`All enemies have been defeated.`)
+            return true
+        }
+    }
+}
+
+function teamAttack (attackers, defenders) {
+    let totalIncapacitated = 0
+    const totalAvailableDefenders = 0
+    defenders.forEach(defender => {
+        if(!defender.isIncapacitated){
+            totalAvailableDefenders++
+        }
+    })
+    attackers.forEach(attacker => {
+        if(attacker.isIncapacitated || totalAvailableDefenders === 0){
+            return
+        }
+
+        let target, randomTargetIndex
+
+        while(!target) {
+            randomTargetIndex = Math.floor(Math.random() * defenders.length)
+
+            if(!defenders[randomTargetIndex].isIncapacitated){
+                target = defenders[randomTargetIndex]
+            }
+        }
+        if(attacker.attack() >= target.barriers.attack){
+            let damage = attacker.dealDamage()
+            target.currentHealth -= damage
+            console.log(`${attacker.name} (Current Health: ${attacker.currentHealth}) hit ${target.name} for ${damage} (current health: ${target.currentHealth})`)
+            if(target.currentHealth <= 0){
+                console.log(`${target.name} is incapacitated!`)
+                target.isIncapacitated = true
+                totalIncapacitated++
+                totalAvailableDefenders--
+            }
+        } else {
+            console.log(`${attacker.name} missed!`)
+        }
+    })
+    return totalIncapacitated
+}
